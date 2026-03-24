d5fde28550a55d0ab4d4295c71a41608b35bdf29
roshani <roshniyadav72006@gmail.com>
2026-03-25 00:03:43 +0530

    fix import path error

--- app/api/admin/bookings/[id]/route.js
+++ app/api/admin/bookings/[id]/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import connectDB from "../../../../../lib/mongoose";
-import Booking from "../../../models/Booking";
+import Booking from "../../../../../models/Booking";
 
 export async function PATCH(req, { params }) {
   await connectDB();
